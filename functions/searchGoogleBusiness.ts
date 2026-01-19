7ac52811fa1be1bff23fb25aede3fa65c7b720ad
Mr. Gee <jtgee101@gmail.com>
2026-01-19 04:19:19 +0000

    File changes

--- functions/searchGoogleBusiness.ts
+++ functions/searchGoogleBusiness.ts
@@ -3,12 +3,6 @@ import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
 Deno.serve(async (req) => {
   try {
     const base44 = createClientFromRequest(req);
-    const user = await base44.auth.me();
-
-    if (!user) {
-      return Response.json({ error: 'Unauthorized' }, { status: 401 });
-    }
-
     const { searchQuery } = await req.json();
 
     if (!searchQuery) {
