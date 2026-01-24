6b176ad844fc0fd665d01b0945f3081da770a0cb
Mr. Gee <jtgee101@gmail.com>
2026-01-24 17:21:12 +0000

    File changes

--- src/components/admin/EmailAnalyticsDashboard.jsx
+++ src/components/admin/EmailAnalyticsDashboard.jsx
@@ -329,7 +329,7 @@ export default function EmailAnalyticsDashboard() {
             </div>
           </CardContent>
         </Card>
-        </motion.div>
+      </motion.div>
       )}
 
       {/* Daily Trend */}
