7a5dc6a601114fc1ac8188afc1868301fa092348
jtgee101-maker <jtgee101@gmail.com>
2026-03-09 10:49:15 -0400

    Flatten page routing and remove nested ghost page files

--- src/pages.config.lazy.js
+++ src/pages.config.lazy.js
@@ -13,12 +13,19 @@ import __Layout from './Layout.jsx';
 
 // Lazy load all pages for code splitting
 const ABTestDashboard = lazy(() => import('./pages/ABTestDashboard'));
+const APILogs = lazy(() => import('./pages/APILogs'));
 const Admin = lazy(() => import('./pages/Admin'));
 const AdminAuditPlan = lazy(() => import('./pages/AdminAuditPlan'));
-const AdminDashboard = lazy(() => import('./pages/admin/AdminDashboard'));
-const AdminTenants = lazy(() => import('./pages/admin/AdminTenants'));
-const AdminUsers = lazy(() => import('./pages/admin/AdminUsers'));
+const AdminAuthCallback = lazy(() => import('./pages/AdminAuthCallback'));
+const AdminDashboard = lazy(() => import('./pages/AdminDashboard'));
+const AdminTenants = lazy(() => import('./pages/AdminTenants'));
+const AdminUsers = lazy(() => import('./pages/AdminUsers'));
+const AdminJobs = lazy(() => import('./pages/AdminJobs'));
+const AdminLogin = lazy(() => import('./pages/AdminLogin'));
+const AdminLogout = lazy(() => import('./pages/AdminLogout'));
+const AdminSystem = lazy(() => import('./pages/AdminSystem'));
 const AdminControlCenter = lazy(() => import('./pages/AdminControlCenter'));
+const BrandEditor = lazy(() => import('./pages/BrandEditor'));
 const adminDeprecated = lazy(() => import('./pages/Admin_DEPRECATED'));
 const AnalyticsVerification = lazy(() => import('./pages/AnalyticsVerification'));
 const AutoBodyLanding = lazy(() => import('./pages/AutoBodyLanding'));
@@ -26,9 +33,12 @@ const AutoRepairLanding = lazy(() => import('./pages/AutoRepairLanding'));
 const BridgeGeenius = lazy(() => import('./pages/BridgeGeenius'));
 const BridgeV3 = lazy(() => import('./pages/BridgeV3'));
 const CampaignTesting = lazy(() => import('./pages/CampaignTesting'));
+const ChaosTestDashboard = lazy(() => import('./pages/ChaosTestDashboard'));
 const Checkout = lazy(() => import('./pages/Checkout'));
 const CheckoutV2 = lazy(() => import('./pages/CheckoutV2'));
 const ChiropractorLanding = lazy(() => import('./pages/ChiropractorLanding'));
+const CityNicheLanding = lazy(() => import('./pages/CityNicheLanding'));
+const ClientDashboard = lazy(() => import('./pages/ClientDashboard'));
 const ContractorsLanding = lazy(() => import('./pages/ContractorsLanding'));
 const CustomDomainGuide = lazy(() => import('./pages/CustomDomainGuide'));
 const Dashboard = lazy(() => import('./pages/Dashboard'));
@@ -42,7 +52,9 @@ const ElectriciansLanding = lazy(() => import('./pages/ElectriciansLanding'));
 const Error403 = lazy(() => import('./pages/Error403'));
 const Error404 = lazy(() => import('./pages/Error404'));
 const Error500 = lazy(() => import('./pages/Error500'));
+const FeatureFlags = lazy(() => import('./pages/FeatureFlags'));
 const Features = lazy(() => import('./pages/Features'));
+const EnterpriseMode = lazy(() => import('./pages/EnterpriseMode'));
 const FinalLaunchChecklist = lazy(() => import('./pages/FinalLaunchChecklist'));
 const FoxyAuditLanding = lazy(() => import('./pages/FoxyAuditLanding'));
 const GettingStarted = lazy(() => import('./pages/GettingStarted'));
@@ -53,8 +65,11 @@ const HVACLanding = lazy(() => import('./pages/HVACLanding'));
 const HandymanLanding = lazy(() => import('./pages/HandymanLanding'));
 const Home = lazy(() => import('./pages/Home'));
 const LandscapingLanding = lazy(() => import('./pages/LandscapingLanding'));
+const LaunchCommandCenter = lazy(() => import('./pages/LaunchCommandCenter'));
 const LawnCareLanding = lazy(() => import('./pages/LawnCareLanding'));
 const LegalLanding = lazy(() => import('./pages/LegalLanding'));
+const MoltBotDemo = lazy(() => import('./pages/MoltBotDemo'));
+const Monitoring = lazy(() => import('./pages/Monitoring'));
 const PlumbersLanding = lazy(() => import('./pages/PlumbersLanding'));
 const PowerWashingLanding = lazy(() => import('./pages/PowerWashingLanding'));
 const PreDeploymentAudit = lazy(() => import('./pages/PreDeploymentAudit'));
@@ -65,7 +80,9 @@ const ProductionChecklist = lazy(() => import('./pages/ProductionChecklist'));
 const ProductionFinalChecklist = lazy(() => import('./pages/ProductionFinalChecklist'));
 const ProductionLaunchChecklist = lazy(() => import('./pages/ProductionLaunchChecklist'));
 const ProductionReadinessCheck = lazy(() => import('./pages/ProductionReadinessCheck'));
+const ProgrammaticAnalytics = lazy(() => import('./pages/ProgrammaticAnalytics'));
 const Quiz = lazy(() => import('./pages/Quiz'));
+const QuickStartWizard = lazy(() => import('./pages/QuickStartWizard'));
 const QuizGeenius = lazy(() => import('./pages/QuizGeenius'));
 const QuizGeeniusV2 = lazy(() => import('./pages/QuizGeeniusV2'));
 const QuizV2 = lazy(() => import('./pages/QuizV2'));
@@ -76,9 +93,12 @@ const RestaurantsLanding = lazy(() => import('./pages/RestaurantsLanding'));
 const ResultsGeenius = lazy(() => import('./pages/ResultsGeenius'));
 const Roadmap = lazy(() => import('./pages/Roadmap'));
 const RoofersLanding = lazy(() => import('./pages/RoofersLanding'));
+const SEOAuditIndex = lazy(() => import('./pages/SEOAuditIndex'));
 const SecurityAudit = lazy(() => import('./pages/SecurityAudit'));
 const Settings = lazy(() => import('./pages/Settings'));
 const StripeSetupGuide = lazy(() => import('./pages/StripeSetupGuide'));
+const SystemHealth = lazy(() => import('./pages/SystemHealth'));
+const TenantManager = lazy(() => import('./pages/TenantManager'));
 const Terms = lazy(() => import('./pages/Terms'));
 const Testing = lazy(() => import('./pages/Testing'));
 const TestingChecklist = lazy(() => import('./pages/TestingChecklist'));
@@ -87,15 +107,23 @@ const Upsell = lazy(() => import('./pages/Upsell'));
 const Upsell1 = lazy(() => import('./pages/Upsell1'));
 const UserJourneyTest = lazy(() => import('./pages/UserJourneyTest'));
 const V2Start = lazy(() => import('./pages/V2Start'));
+const GrowthMode = lazy(() => import('./pages/GrowthMode'));
 
 export const PAGES = {
     "ABTestDashboard": ABTestDashboard,
+    "APILogs": APILogs,
     "Admin": Admin,
     "AdminAuditPlan": AdminAuditPlan,
+    "AdminAuthCallback": AdminAuthCallback,
     "AdminDashboard": AdminDashboard,
     "AdminTenants": AdminTenants,
     "AdminUsers": AdminUsers,
+    "AdminJobs": AdminJobs,
+    "AdminLogin": AdminLogin,
+    "AdminLogout": AdminLogout,
+    "AdminSystem": AdminSystem,
     "AdminControlCenter": AdminControlCenter,
+    "BrandEditor": BrandEditor,
     "Admin_DEPRECATED": adminDeprecated,
     "AnalyticsVerification": AnalyticsVerification,
     "AutoBodyLanding": AutoBodyLanding,
@@ -103,9 +131,12 @@ export const PAGES = {
     "BridgeGeenius": BridgeGeenius,
     "BridgeV3": BridgeV3,
     "CampaignTesting": CampaignTesting,
+    "ChaosTestDashboard": ChaosTestDashboard,
     "Checkout": Checkout,
     "CheckoutV2": CheckoutV2,
     "ChiropractorLanding": ChiropractorLanding,
+    "CityNicheLanding": CityNicheLanding,
+    "ClientDashboard": ClientDashboard,
     "ContractorsLanding": ContractorsLanding,
     "CustomDomainGuide": CustomDomainGuide,
     "Dashboard": Dashboard,
@@ -119,7 +150,9 @@ export const PAGES = {
     "Error403": Error403,
     "Error404": Error404,
     "Error500": Error500,
+    "FeatureFlags": FeatureFlags,
     "Features": Features,
+    "EnterpriseMode": EnterpriseMode,
     "FinalLaunchChecklist": FinalLaunchChecklist,
     "FoxyAuditLanding": FoxyAuditLanding,
     "GettingStarted": GettingStarted,
@@ -129,8 +162,11 @@ export const PAGES = {
     "HandymanLanding": HandymanLanding,
     "Home": Home,
     "LandscapingLanding": LandscapingLanding,
+    "LaunchCommandCenter": LaunchCommandCenter,
     "LawnCareLanding": LawnCareLanding,
     "LegalLanding": LegalLanding,
+    "MoltBotDemo": MoltBotDemo,
+    "Monitoring": Monitoring,
     "PlumbersLanding": PlumbersLanding,
     "PowerWashingLanding": PowerWashingLanding,
     "PreDeploymentAudit": PreDeploymentAudit,
@@ -141,7 +177,9 @@ export const PAGES = {
     "ProductionFinalChecklist": ProductionFinalChecklist,
     "ProductionLaunchChecklist": ProductionLaunchChecklist,
     "ProductionReadinessCheck": ProductionReadinessCheck,
+    "ProgrammaticAnalytics": ProgrammaticAnalytics,
     "Quiz": Quiz,
+    "QuickStartWizard": QuickStartWizard,
     "QuizGeenius": QuizGeenius,
     "QuizGeeniusV2": QuizGeeniusV2,
     "QuizV2": QuizV2,
@@ -152,9 +190,12 @@ export const PAGES = {
     "ResultsGeenius": ResultsGeenius,
     "Roadmap": Roadmap,
     "RoofersLanding": RoofersLanding,
+    "SEOAuditIndex": SEOAuditIndex,
     "SecurityAudit": SecurityAudit,
     "Settings": Settings,
     "StripeSetupGuide": StripeSetupGuide,
+    "SystemHealth": SystemHealth,
+    "TenantManager": TenantManager,
     "Terms": Terms,
     "Testing": Testing,
     "TestingChecklist": TestingChecklist,
@@ -163,6 +204,7 @@ export const PAGES = {
     "Upsell1": Upsell1,
     "UserJourneyTest": UserJourneyTest,
     "V2Start": V2Start,
+    "GrowthMode": GrowthMode,
 }
 
 export const pagesConfig = {
